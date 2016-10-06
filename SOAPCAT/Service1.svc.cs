06757ac77d890028b6c082939f93719927f6dbfc
TRiBByX <christoffer.ssj@gmail.com>
2016-10-06 23:21:28 +0200

    Small comment changes. nothing important.

--- SOAPCAT/Service1.svc.cs
+++ SOAPCAT/Service1.svc.cs
@@ -8,8 +8,6 @@ using System.Text;
 
 namespace SOAPCAT
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CatService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select CatService.svc or CatService.svc.cs at the Solution Explorer and start debugging.
     public class CatService : ICatService
     {
         public Cat GetCat(Cat c)
